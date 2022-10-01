bc5f4380ce4812ce501e33fe4db0f74b60071dea
Abdullah Younus <abdullah1001k@gmail.com>
2022-10-01 05:06:03 -0700

    alien colour three

---
+++ alienColourThree/index.js
@@ -0,0 +1,9 @@
+let alien_colour = "red";
+
+if (alien_colour === "green") {
+    document.writeln("You Just Earned 5 Points!");
+} else if (alien_colour === "yellow") {
+    document.writeln("You Just Earned 10 Points!");
+} else {
+    document.writeln("You Just Earned 15 Points!");
+}
