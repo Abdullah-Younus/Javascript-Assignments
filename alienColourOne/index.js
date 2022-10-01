522bcdaeba4788c358de00efef2f61a19606698b
Abdullah Younus <abdullah1001k@gmail.com>
2022-10-01 04:52:36 -0700

    alien colour done

---
+++ alienColourOne/index.js
@@ -0,0 +1,11 @@
+alien_color = "green";
+if (alien_color == "green") {
+    document.writeln(`${alien_color}  Your just earned 5 points!`)
+}
+
+// failing version
+alien_color = "red";
+
+if (alien_color == "green") {
+    document.writeln(`Your just earned 5 points!`)
+}
