bf3442921420821e461485fda3c06dd321e386c4
Abdullah Younus <abdullah1001k@gmail.com>
2022-10-01 05:37:18 -0700

    favourite fruite

---
+++ favouriteFruit/index.js
@@ -0,0 +1,20 @@
+let favorite_fruits = ['apple', 'mango', 'banana']
+
+
+for (let i = 0; i < favorite_fruits.length; i++) {
+    if (favorite_fruits[i] === "Coconut") {
+        document.writeln(`Your Really Like ${favorite_fruits[i]}!`);
+    } else if (favorite_fruits[i] === "Custard apple") {
+        document.writeln(`Your Really Like ${favorite_fruits[i]}!`);
+
+    } else if (favorite_fruits[i] === "Dates") {
+        document.writeln(`Your Really Like ${favorite_fruits[i]}!`);
+
+    } else if (favorite_fruits[i] === "apple") {
+        document.writeln(`Your Really Like ${favorite_fruits[i]}!` + "<br/>");
+
+    } else if (favorite_fruits[i] === "mango") {
+        document.writeln(`Your Really Like ${favorite_fruits[i]}!`);
+
+    }
+}
