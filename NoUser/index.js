47869aaba964642291665c55356a9b4954bb3de6
Abdullah Younus <abdullah1001k@gmail.com>
2022-10-01 06:08:08 -0700

    user no

---
+++ NoUser/index.js
@@ -0,0 +1,14 @@
+let usernames = []
+
+if (usernames === null) {
+    for (let i = 0; i <= usernames.length; i++) {
+        if (usernames[i] === "admin") {
+            document.writeln(`Hello ${usernames}, would you like to see a status report?`);
+        }
+        else {
+            document.writeln(`Hello ${username}, thank you for loggin in again`);
+        }
+    }
+} else {
+    document.writeln("We Need to find some users!");
+}
