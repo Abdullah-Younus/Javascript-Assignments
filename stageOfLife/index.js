1d9ba661f8cfc5d478ad7b07dbe4ffa83be0f39c
Abdullah Younus <abdullah1001k@gmail.com>
2022-10-01 05:14:55 -0700

    stage of life

---
+++ stageOfLife/index.js
@@ -0,0 +1,15 @@
+let age = 15;
+
+if (age < 2) {
+    document.writeln(`You're a baby!`);
+} else if (age < 4) {
+    document.writeln(`You're a toddler!`);
+} else if (age < 13) {
+    document.writeln(`You're a kid!`);
+} else if (age < 20) {
+    document.writeln(`You're a teenager!`);
+} else if (age < 65) {
+    document.writeln(`You're an adult!`);
+} else {
+    document.writeln(`You're an elder`);
+}
