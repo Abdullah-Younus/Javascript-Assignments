d4d6270d3e2ac8225b697eafb21960c603d69dd3
Abdullah Younus <abdullah1001k@gmail.com>
2022-10-01 13:43:23 -0700

    done

--- Album/index.js
+++ Album/index.js
@@ -1,5 +1,5 @@
 function make_album(artist, title) {
-    document.writeln(`Build a Dictionary Containing Information About An Album` + "<br/>");
+    document.writeln(`Information About An Album` + "<br/>");
     var album_list = {
         "artist": artist,
         "title": title
@@ -9,3 +9,24 @@ function make_album(artist, title) {
 
 alb = make_album("Atif Aslam", "It's an amazing Singer");
 document.writeln(JSON.stringify(alb), "<br/>");
+
+
+/// With Tracks
+document.writeln(`<hr/>`);
+document.writeln(`<b>Tracks</b>`);
+document.writeln(`<hr/>`);
+
+function make_album(artist, title, tracks = 0) {
+    document.writeln(`Information About An Album ` + "<br/>");
+    var album_list = {
+        'artist': artist,
+        'title': title
+    }
+    if (tracks) {
+        album_list['tracks'] = tracks
+    }
+    return album_list;
+}
+
+alb = make_album("Badshah", "He is Creating Rock Songs");
+document.writeln(JSON.stringify(alb));
